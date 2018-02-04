04362104d0f4dc82f9450073efe810a7f533a22e
Richard <i59709063202@gmail.com>
2018-02-04 13:47:22 +0800

    finish

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,3 +1,14 @@
-export const stars = (n) => {
-  return;
+function stars (n){
+
+  var arr=[]
+  for(var i=0;i<n;i++){
+    arr.push('*')
+  }
+  return arr
 }
+
+describe("hw1_1",function() {
+  it("should return correct answer when n = 1",function(){
+    expect(stars(1)).toEqual(['*'])
+  })
+})
